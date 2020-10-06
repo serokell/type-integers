3737a7e744b3a8c5f1a02472d1812ea3f484a7d2
Preetham Gujjula <preetham.gujjula@serokell.io>
2020-10-05 19:22:03 -0600

    [#3] Comment out custom typeclasses
    Problem: The custom typeclasses IsCommutativeRing and IsInteger are
    likely inspired by the IsPeano and PeanoOrder typeclasses in
    type-natural. However, the latter typeclasses are necessary to unify the
    proofs for the built-in and library Nat types. There is no such
    unification necessary for type-integers, so these typeclasses just add
    unnecessary complexity at the moment.
    
    Solution: Comment out the typeclasses, leaving open the possibility of
    reintroducing them into the code if they become useful later.

--- src/Data/Type/Zahlen.hs
+++ src/Data/Type/Zahlen.hs
@@ -11,14 +11,14 @@ import Data.Type.Natural (Nat (S, Z))
 import Data.Type.Zahlen.Class
 import Data.Type.Zahlen.Definitions
 
-instance IsCommutativeRing Zahlen where
-  type Zero' = ('Pos 'Z)
-  type One' = ('Pos (S Z))
-  type Inv m = Inverse m
+--instance IsCommutativeRing Zahlen where
+--  type Zero' = ('Pos 'Z)
+--  type One' = ('Pos (S Z))
+--  type Inv m = Inverse m
 --   zeroIdentity :: forall x m. Absolute'' x :~: 'Z -> x + m :~: m
 --   zeroIdentity Refl = Refl `because` (Proxy )
 
-instance IsInteger Zahlen where
-  type Signum ('Pos m) = P
-  type Signum ('Neg m) = N
-  type Absolute'' (_ m) = m
+--instance IsInteger Zahlen where
+--  type Signum ('Pos m) = P
+--  type Signum ('Neg m) = N
+--  type Absolute'' (_ m) = m
